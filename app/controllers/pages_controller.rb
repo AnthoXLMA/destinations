5939a1e2d05fc38477cdb375b15f89a2cb67a2b4
AnthoXLMA <contact@service-lite-plus.com>
2023-08-01 10:24:53 +0200

    destinationsviews

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  def home
+  end
+
   def about
+  end
+
+  def contact
+    @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+  end
+
+  def destinations
+    @destinations = ["Saint-Tropez", "Courchevel", "Saint-barthélémy", "Miami", "Saint-Martin"]
   end
 end
