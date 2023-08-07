39f15d1c425a4a218580548f5d8ea4eabe01a264
AnthoXLMA <contact@service-lite-plus.com>
2023-08-07 09:34:36 +0200

    rubriques display OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,9 +13,28 @@ class PagesController < ApplicationController
     @trips = ["Saint-Tropez", "Courchevel", "Saint-barthélémy", "Miami", "Saint-Martin"]
     @destinations = Destination.all
     @themes = Theme.all
+    @rubriques = Rubrique.all
+    @display = @rubriques.each do |display|
+      display.chapter
+    end
   end
 
   def themes
     @themes = Theme.all
+    @rubriques = Rubrique.all
+  end
+
+  def rubriques
+    @rubriques = Rubrique.all
+  end
+
+  private
+
+  def set_rubrique
+    @rubrique = Rubrique.find(params[:theme_id])
+  end
+
+  def rubrique_params
+    params.require(:rubrique).permit(:theme_id, :chapter)
   end
 end
